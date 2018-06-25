833e09d3dce66cc01d2eb8c5774b3d8647fa0ed3
Jake Zimmerman <zimmerjm@rose-hulman.edu>
2018-06-25 13:36:46 -0400

    change text of heading

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const button = document.querySelector('button')
+const changeText = function() {
+    const heading = document.querySelector('h1')
+    heading.textContent = "Bunch of Chrismesses"
+}
+
+button.addEventListener('click', changeText)
