4fc5b1b83764f0111d1eb66151a027ce5fb9d3b3
Jake Zimmerman <zimmerjm@rose-hulman.edu>
2018-06-25 15:13:03 -0400

    Form Input will change a header

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const button = document.querySelector('button')
+const button = document.querySelector('#change')
 const changeText = function() {
     const heading = document.querySelector('#exciting')
     heading.textContent = "Bunch of Chrismesses"
 }
-
 button.addEventListener('click', changeText)
+
+const submit = document.querySelector('#submit')
+
+function changer() {
+    const form = document.querySelector('form')
+    const text = document.querySelector('#input').value
+    if(text == '') {
+            alert("enter value to change header!")
+            return false;
+    }
+    const origHeading = document.querySelector('#exciting')
+    origHeading.textContent = text
+}
+
+submit.addEventListener('click', changer)
