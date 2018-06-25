7e2ec62173c262d31e2f64c9512ac0366b86ff75
Jake Zimmerman <zimmerjm@rose-hulman.edu>
2018-06-25 13:41:19 -0400

    added multiple headings and changed the second one

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const button = document.querySelector('button')
 const changeText = function() {
-    const heading = document.querySelector('h1')
+    const heading = document.querySelector('#exciting')
     heading.textContent = "Bunch of Chrismesses"
 }
 
